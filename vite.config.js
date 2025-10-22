c8116fb8b348f1fe204f18c15d6d152be5eea3ed
Elias <eliascorrea@4nlogistica.cl>
2025-10-22 11:43:39 -0300

    Se optimiza archivo css del panel de Finanzas

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
                 'resources/css/login.css',
                 'resources/css/dashboard.css',
                 'resources/css/appcustom.css',
+                'resources/css/panel-finanzas.css',
                 'resources/sass/app.scss',
                 'resources/js/app.js',  // Mantén el archivo JS si lo usas
             ],
