cc21cdd8686f1a54390ff7ceb2be861906f0d6ff
Elias <eliascorrea@4nlogistica.cl>
2026-01-18 13:01:59 -0300

    Se agrega otro usuario en trackin para ver

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 @php
-    $trackingOnlyUsers = [1, 4, 8, 22, 30, 36,375, 14];
+    $trackingOnlyUsers = [1, 4, 8, 22, 30, 36,375, 14, 27];
     $isTrackingOnlyUser = auth()->check() && in_array(auth()->id(), $trackingOnlyUsers);
 @endphp
 
