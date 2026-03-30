45c60f818128dcf854c90139d6fde000362ca701
Elias <eliascorreap@gmail.com>
2026-03-29 22:07:02 -0300

    Se cambia la ruta raiz, ahora será la b2b

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/', function () {
         return redirect('/empleados/perfil');
     }
 
-    return view('landing');
+    return redirect('/sitio-4n');
 })->name('home');
 
 Route::get('/sitio-4n', function () {
